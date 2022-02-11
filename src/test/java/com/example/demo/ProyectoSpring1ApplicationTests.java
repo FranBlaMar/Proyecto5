bbb75094c841dd7a4e2d6f7acf71c0625134f071
FranBlaMar <74529306+FranBlaMar@users.noreply.github.com>
2022-02-11 10:48:16 +0100

    Update ProyectoSpring1ApplicationTests.java

--- src/test/java/com/example/demo/ProyectoSpring1ApplicationTests.java
+++ src/test/java/com/example/demo/ProyectoSpring1ApplicationTests.java
@@ -1,19 +1,26 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.demo.model.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.PedidoService;
 
 @SpringBootTest
 class ProyectoSpring1ApplicationTests {
-
+@Autowired
+	private PedidoService servicio;
+	
+	
 	@Test
 	void contextLoads() {
 	}
 	
 	@Test
 	  void testFallo() {
-	    assertTrue(false);
+		Pedido p = this.servicio.borrarPedido(2);
+	    assertNotNull(p);
 	 }
-
 }
